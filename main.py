f664fba09f8b2f3412bf0c49539d74989339bfb1
sorakunn <sorasunn@163.com>
2018-05-09 02:15:45 +0800

    Function split_a for cut out the User_ID and Article_ID from the url, meanwhile, clean the data by cut off the unwanted string.

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-from preprocess.PP import *
+from tools.IO import read
+from surprise import Reader
+from surprise import Dataset
+from surprise import KNNBasic
+from surprise.model_selection import KFold
+from evaluation import PR
 
-merge()
+FILE_PATH= 'Data/Friend/Result/User_Friend_Mutual3.txt'
+# 读取数据
+reader = Reader(line_format='user item rating', sep=' ', rating_scale =(0,1))
+data = Dataset.load_from_df(read(FILE_PATH), reader=reader)
 
-
-
-
-# from tools.IO import read
-# from surprise import Reader
-# from surprise import Dataset
-# from surprise import KNNBasic
-# from surprise.model_selection import KFold
-# from evaluation import PR
-#
-# FILE_PATH= 'Data/Friend/Result/User_Friend_Mutual3.txt'
-# # 读取数据
-# reader = Reader(line_format='user item rating', sep=' ', rating_scale =(0,1))
-# data = Dataset.load_from_df(read(FILE_PATH), reader=reader)
-#
-# kf = KFold(n_splits=5)
-# # 使用算法
-# algo = KNNBasic()
-# for trainset, testset in kf.split(data):
-#     algo.fit(trainset)
-#     predictions = algo.test(testset)
-#     precisions, recalls = PR.precision_recall_at_k(predictions, k=5, threshold=4)
-#     # Precision and recall can then be averaged over all users
-#     print(sum(prec for prec in precisions.values()) / len(precisions))
-#     print(sum(rec for rec in recalls.values()) / len(recalls))
+kf = KFold(n_splits=5)
+# 使用算法
+algo = KNNBasic()
+for trainset, testset in kf.split(data):
+    algo.fit(trainset)
+    predictions = algo.test(testset)
+    precisions, recalls = PR.precision_recall_at_k(predictions, k=5, threshold=4)
+    # Precision and recall can then be averaged over all users
+    print(sum(prec for prec in precisions.values()) / len(precisions))
+    print(sum(rec for rec in recalls.values()) / len(recalls))
--- preprocess/PP.py
+++ preprocess/PP.py
@@ -1,15 +1,20 @@
 from tools.IO import *
 
 
-def merge(subject, amount):
+def merge(datatype,subject, amount):
     """读取多个单份文件，并合并返回"""
+    # datatype 对应 friend 数据和 atricle 数据两种情况
+    if datatype == 'f':
+        datatype = 'Friend'
+    elif datatype == 'a':
+        datatype = 'Article'
     # 生成存放结构
     df = pd.DataFrame()
     # 对于被分成的每一份
     for i in range(1, amount + 1):
         # 指定根路径
-        root = 'Data/Friend/Source/' + subject + '/'
-        # 指定单份路径
+        root = 'Data/' + datatype + '/Source/' + subject + '/'
+        # 指定单份路径p;
         if amount == 1:
             filename = root + subject + '.csv'
         else:
@@ -20,11 +25,11 @@ def merge(subject, amount):
     return df
 
 
-def split(subject, amount, null_reserved):
+def split_f(subject, amount, null_reserved):
     """从url中分离出user_ID与friend_ID"""
 
     # 合并数据
-    merged = merge(subject, amount)
+    merged = merge('f',subject, amount)
     # 截取 ID (分要不要空值两种情况)
     df = [['博主ID','好友ID']]
     for index,row in merged.iterrows():
@@ -38,7 +43,7 @@ def split(subject, amount, null_reserved):
     write(df,'Data/Friend/Result/Raw/' + subject + '.csv')
 
 
-def count(subject):
+def count_f(subject):
     """对每位user的好友个数进行计数"""
     # 读取数据
     df = read('Data/Friend/Result/Raw/' + subject + '.csv')
@@ -60,3 +65,49 @@ def count(subject):
     # 写出文件
     df = pd.DataFrame(data=result)
     write(df,'Data/Friend/Result/Count/' + subject + '.csv')
+
+
+def split_a(subject,amount,year):
+    # 合并数据
+    merged = merge('a', subject, amount)
+    # 定义存放列表
+    url = []                                                    # 存放未截断的 user_ID，article_ID
+    user_ids = []                                               # 存放未截断的 user_ID
+    article_ids = []                                            # 存放未截断的 article_ID
+    index_of_target = []                                          # 存放时间为指定年份的数据行
+    # 数据清洗
+    for index,row in merged.iterrows():
+        # 阅读次数
+        if row[1]==row[1]:
+            row[1] = row[1][3:-4]
+        # 文章时间（挑选出属于指定年份的行号）
+        if row[2]==row[2]:
+            if row[2][:4] == str(year):
+                index_of_target.append(index)
+        # 系统标签、用户标签（如果系统标签不存在，则将个人标签修正为系统标签）
+        if row[4]!=row[4]:
+            row[4] = row[3]
+            row[3] = None
+        # 分割用户ID、文章ID
+        if row[7].find('-')!=-1:
+            url = row[7][31:-5].split('-')
+            user_ids.append(url[0])
+            article_ids.append(url[1])
+        else:
+            user_ids.append(-1)
+            article_ids.append(-1)
+        # 删除正文尾部转载声明
+        if row[6] == row[6]:
+            if row[6].rfind('转载本文请') != -1:
+                index = row[6].rindex('转载本文请')
+                row[6] = row[6][:index]
+    # 弹出 url 列，添加 User_ID，Article_ID 列
+    merged.pop('页面网址')
+    merged['用户ID'] = user_ids
+    merged['文章ID'] = article_ids
+    # 通过之前存入的index数组，提取出需要的数据行
+    df = pd.DataFrame(columns=["标题", "阅读次数", "时间", "个人分类", "系统分类", "标签", "正文", "用户ID", "文章ID"])
+    for index in index_of_target:
+        df = df.append(merged.ix[index:index])               # 注意要将append后的数据传回
+    # 写出数据
+    write(df,'Data/Article/Result/Raw/' + subject + '.csv')
